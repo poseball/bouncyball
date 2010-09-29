335ece50797c17aa26964d4e7fa6f465b978e7d1
unknown <Argamasilla@.(none)>
2010-09-28 21:44:33 -0700

    bouncy ball rails initial commit

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'ca8b5d67072a9bd813f6851a9d6cedce'
+end
